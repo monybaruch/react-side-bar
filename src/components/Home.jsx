9dcdb155657b1935b29d6c9ff941e4d2d6dd602c
מוני ברוך <monybaruch@gmail.com>
2023-09-19 19:56:16 +0300

    adding the styling for the navbar and the toggle modal buttons and a bounce animation for the navbar button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from '../context/context';
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
       </button>
     </main>
   );
