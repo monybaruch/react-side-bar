3b1c3295aee4432db4e2816b03cc41a601bb8231
מוני ברוך <monybaruch@gmail.com>
2023-09-19 20:36:23 +0300

    working on the modal component and setting up the css and the login for hidding and showing up the overlay

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,17 @@
 import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from '../context/context';
 
 const Model = () => {
-  return <div>Model</div>;
+  const { isModelOpen, closeModal } = useGlobalContext();
+  return (
+    <div className={isModelOpen ? 'model-overlay show-modal' : 'model-overlay'}>
+      <div className="model-container">
+        <h3>model content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Model;
