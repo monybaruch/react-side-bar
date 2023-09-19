966fbc8d1d9c4d7d0d4db114d5519fc7011ed42c
מוני ברוך <monybaruch@gmail.com>
2023-09-19 21:23:28 +0300

    adding the css and the logic for the sidebar for the toggle button and with this the project is done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,41 @@
+import { useGlobalContext } from '../context/context';
 import { FaTimes } from 'react-icons/fa';
 import { social, links } from '../data';
-
+import logo from '../logo.svg';
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo"></img>
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 export default Sidebar;
